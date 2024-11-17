d8db4cbbe511c156603fc12a6b2f1dc8585984e8
Navinkumar Singh <singhnavinkumar493@gmail.com>
2024-11-17 19:30:21 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import {BrowserRouter,Route,Routes} from "react-router-dom"
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
+import SellerDashboard from "./pages/SellerDashboard"
+import BuyerDashboard from "./pages/BuyerDashboard"
+import Navbar from "./components/Navbar"
+
+
+export default function App() {
+  return (
+    <>
+    <BrowserRouter>
+    <Navbar/>
+      <Routes>
+        <Route path="/" element={ <Home/>} />
+        <Route path="/login" element={ <Login/>} />
+        <Route path="/signup" element={ <Signup/>} />
+        <Route path="/seller/profile" element={ <SellerDashboard/>} />
+        <Route path="/buyer/profile" element={ <BuyerDashboard/>} />
+      </Routes>
+    </BrowserRouter>
+
+
+    </>
+  )
+}
---
+++ src/pages/SellerDashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SellerDashboard = () => {
+  return (
+    <div>
+      SELLER
+    </div>
+  )
+}
+
+export default SellerDashboard
